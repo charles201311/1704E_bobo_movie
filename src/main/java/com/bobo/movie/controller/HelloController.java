b5f9889a65fdf6bec9404e2b05f50add69353759
bobo <348828809@qq.com>
2019-08-07 11:17:26 +0800

    添加了hellocontroller

---
+++ src/main/java/com/bobo/movie/controller/HelloController.java
@@ -0,0 +1,23 @@
+/**   
+ * Copyright © 2019 公司名. All rights reserved.
+ * 
+ * @Title: HelloController.java 
+ * @Prject: bobo-movie
+ * @Package: com.bobo.movie.controller 
+ * @Description: TODO
+ * @author: charles   
+ * @date: 2019年8月7日 上午11:16:31 
+ * @version: V1.0   
+ */
+package com.bobo.movie.controller;
+
+/** 
+ * @ClassName: HelloController 
+ * @Description: TODO
+ * @author: charles
+ * @date: 2019年8月7日 上午11:16:31  
+ */
+public class HelloController {
+
+	//-----------111111111111111111111
+}
